190fa0b5e0f2df04d5f23ba7353419db827cac34
Saket <saketclasher@gmail.com>
2023-02-02 13:28:42 +0530

    stop function bug, layout change and functionality changes

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const LEVEL_DURATIONS = [0.5, 1, 1.5, 2, 2.5, 3];
 const NUM_FLASHES = 12;
 const beepSound = new Audio("media/beep.mp3");
 
+const videoContainer = document.getElementById('start-page');
+const gameContainer = document.getElementById('game-page');
+const gameBox = document.getElementById('game-container');
+const countdownContainer = document.getElementById('countdown');
+
 let startBeepPlayed = false;
 let endBeepPlayed = false;
 let gameInterval;
+let countdownInterval
 let counter = 0;
 let previousColor;
 let countdown = 3;
@@ -24,156 +30,106 @@ function startGame(level) {
     countdown = 3;
     startBeepPlayed = false;
     endBeepPlayed = false;
-    const videoContainer = document.getElementById('video-container');
-    const gameContainer = document.getElementById('game-container');
     if (!videoContainer || !gameContainer) {
         console.error('Error: Elements video-container or game-container not found');
         return;
     }
 
     videoContainer.style.display = 'none';
-    gameContainer.style.backgroundColor = 'transparent';
+    // gameContainer.style.backgroundColor = 'transparent';
     gameContainer.style.display = 'block';
     clearInterval(gameInterval);
-    // for (let i = 0; i < 3; i++){
-    //     setTimeout (() => {
-    //         gameContainer.innerHTML = `<div style="text-align:center;">` + i + `</div>`;
-    //     },1000)
-    // }
-    // if (!startBeepPlayed) {
-    //     beepSound.play(); // Play the start beep sound
-    //     startBeepPlayed = true;
-    // }
 
-    // gameInterval = setInterval(() => {
-    //     flashColors(level);
-    // }, LEVEL_DURATIONS[level - 1] * 1000);
-
-    // flashColors(level);
-
-    // if (!endBeepPlayed) {
-    //     beepSound.play(); // Play the end beep sound
-    //     endBeepPlayed = true;
-    // }
-    const countdownInterval = setInterval(() => {
+    countdownInterval = setInterval(() => {
         if (countdown === 0) {
             clearInterval(countdownInterval);
+
+            gameBox.innerHTML = '';
             if (!startBeepPlayed) {
                 beepSound.play(); // Play the start beep sound
                 startBeepPlayed = true;
             }
             flashColors(level);
         } else {
-            gameContainer.innerHTML = '<img src="media/' + countdown + '.png" alt="Image" height="300px" width="500px">';
+
+            gameBox.innerHTML = countdown;
             console.log(countdown);
             countdown--;
         }
     }, 1000);
 
-    // stopGame();
-
 }
 
 function flashColors(level) {
-    const gameContainer = document.getElementById('game-container');
-    gameContainer.innerHTML = '';
+
     if (!gameContainer) {
         console.error('Error: Element game-container not found');
         return;
     }
-    // for (let i = 0; i < NUM_FLASHES - 1; i++) {
-
+    if (!gameBox) {
+        console.error('Error: Element BOX not found');
+        return;
+    }
     gameInterval = setInterval(() => {
-        // setTimeout(() => {
         let options = COLORS.filter(color => color !== previousColor);
         let color = options[Math.floor(Math.random() * options.length)];
-        gameContainer.style.backgroundColor = color;
+        gameBox.style.backgroundColor = color;
         console.log(counter + ": " + color);
         previousColor = color;
-        // gameContainer.style.backgroundColor = COLORS[Math.floor(Math.random() * COLORS.length)];
-        // }, LEVEL_DURATIONS[level - 1] * 1000);
-        // console.log(counter);
+
         counter++;
-        if (counter >= NUM_FLASHES) {
+        if (counter > NUM_FLASHES) {
             clearInterval(gameInterval);
             stopGame(level);
-            counter = 0;
+
         }
     }, LEVEL_DURATIONS[level - 1] * 1000);
 
-
-    // setTimeout(() => {
-    //     gameContainer.style.backgroundColor = COLORS[Math.floor(Math.random() * COLORS.length)];
-    // }, LEVEL_DURATIONS[level - 1] * 1000);
-    // console.log(counter);
-    // counter++;
-    // if (counter >= NUM_FLASHES){
-    //     clearInterval(gameInterval);
-
-    // }
-    // }
-    // setTimeout(() => {
     if (!gameContainer) {
         console.error('Error: Element game-container not found');
         return;
     }
-    // gameContainer.style.backgroundColor = 'white';
-    const videoContainer = document.getElementById('video-container');
+
     if (!videoContainer) {
         console.error('Error: Element video-container not found');
         return;
     }
-    // clearInterval(gameInterval);
-    // }, LEVEL_DURATIONS[level - 1] * 1000);
-
-    // console.log("Start Beep/Text");
-
-    // setTimeout(() => {
-    //     console.log("End Beep/Text");
-    //     if (!endBeepPlayed) {
-    //         beepSound.play(); // Play the end beep sound
-    //         endBeepPlayed = true;
-    //     }
-    // }, NUM_FLASHES * LEVEL_DURATIONS[level - 1] * 1000);
-
-    // setTimeout(() => {
-    //     const videoContainer = document.getElementById('video-container');
-    //     if (!videoContainer) {
-    //         console.error('Error: Element video-container not found');
-    //         return;
-    //     }
-    //     gameContainer.style.display = 'none';
-    //     videoContainer.style.display = 'block';
-    //     videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xE_fQGaC4vI"></iframe>';
-    // }, (NUM_FLASHES + 1) * LEVEL_DURATIONS[level - 1] * 1000);
-
 }
 
 function stopGame(level) {
+    clearInterval(gameInterval);
+    clearInterval(countdownInterval);
 
+    if (counter > NUM_FLASHES) {
+        gameBox.style.backgroundColor = 'transparent';
+        gameBox.innerHTML = `Congratulations`;
+        console.log("congratulations");
+        counter = 0;
+        setTimeout(() => {
+            gameContainer.style.display = 'none';
+            gameBox.style.backgroundColor = 'transparent';
+            counter = 0;
+            gameBox.innerHTML = '';
+            videoContainer.style.display = 'block';
+            console.log('video display');
+        }, 2000);
+    } else {
+        gameContainer.style.display = 'none';
+        gameBox.style.backgroundColor = 'transparent';
+        counter = 0;
+        gameBox.innerHTML = '';
+        videoContainer.style.display = 'block';
+        console.log('video display');
+    }
     if (!endBeepPlayed) {
         beepSound.play(); // Play the start beep sound
         endBeepPlayed = true;
     }
 
-    const videoContainer = document.getElementById('video-container');
-    const gameContainer = document.getElementById('game-container');
-
-    setTimeout(() => {
-
-        // clearInterval(gameInterval);
-
-        if (!videoContainer) {
-            console.error('Error: Element video-container not found');
-            return;
-        }
-        gameContainer.style.display = 'none';
-        videoContainer.style.display = 'block';
-        console.log('video display');
-        videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xE_fQGaC4vI"></iframe>';
-    }, LEVEL_DURATIONS[level - 1] * 1000);
-
-    // clearInterval(gameInterval);
+    if (!videoContainer) {
+        console.error('Error: Element video-container not found');
+        return;
+    }
 
     if (!gameContainer) {
         console.error('Error: Element game-container not found');
